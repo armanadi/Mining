af2545c290bc9d3a996b7172bf31a1e28ad3052c
armanadi <88218426+armanadi@users.noreply.github.com>
2021-07-31 09:00:18 +0700

    Create dogecoin.sh

---
+++ dogecoin.sh
@@ -0,0 +1,4 @@
+#dogecoin
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
+./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u DHzP2KtxSeSkTnYAw6sTrEguQqYWpgbdED.Arman01 -p x -k --cpu 4
